45a55ce5a538dd371dceec8f5ad1664cb7e8bee9
fouaddakhel96 <fouaddak96@gmail.com>
2020-07-14 13:06:13 +0300

    Updates

---
+++ precache-manifest.fcb5d38ff809c8ba08749c671ad1aa3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1afd1d7fbeca9ce70bedd41a4340092b",
+    "url": "/supermarket/index.html"
+  },
+  {
+    "revision": "e4dd61e3d8fd7494c74a",
+    "url": "/supermarket/static/css/2.260c6bb1.chunk.css"
+  },
+  {
+    "revision": "a9982aa0ccd63ccc484d",
+    "url": "/supermarket/static/css/main.41ef8343.chunk.css"
+  },
+  {
+    "revision": "e4dd61e3d8fd7494c74a",
+    "url": "/supermarket/static/js/2.dc91ba25.chunk.js"
+  },
+  {
+    "revision": "d679e4066658f44ff9c3e023bdda957e",
+    "url": "/supermarket/static/js/2.dc91ba25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9982aa0ccd63ccc484d",
+    "url": "/supermarket/static/js/main.669f565d.chunk.js"
+  },
+  {
+    "revision": "7e392dcfc79aedfb1242",
+    "url": "/supermarket/static/js/runtime-main.9606da99.js"
+  }
+]);
